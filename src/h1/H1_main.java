6171e63262846873b4a49435ce5befb43d0bdb26
Chris24305 <christian.schaa@web.de>
2026-01-12 15:37:42 +0100

    H1V1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,18 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[]args) {
+		Node e = new Node(null);
+		Node d = new Node(e);
+		Node c = new Node(d);
+		Node b = new Node(c);
+		Node a = new Node(b);
+	}
+	
+	public static int distance(Node x, Node y) {
+		if (y==x) {
+			return 0;
+		}
+		return 1 + distance(x.getNext(), y);
+	}
+}
